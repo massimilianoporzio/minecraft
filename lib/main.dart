51318737a0935af47042d30377b7941e28a6cd29
MASSIMILIANO PORZIO <massimiliano.porzio@gmail.com>
2022-11-03 19:29:46 +0100

    MainGame empty

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,9 @@
+import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
+import 'package:minecraft/main_game.dart';
+
+void main() {
+  //*initialize
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(GameWidget(game: MainGame()));
+}
