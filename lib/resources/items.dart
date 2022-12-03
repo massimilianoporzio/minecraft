6cd69d3fea01fa298474e7fe47194236150515cd
MASSIMILIANO PORZIO <massimiliano.porzio@gmail.com>
2022-12-03 10:59:10 +0100

    itemdata

--- lib/resources/items.dart
+++ lib/resources/items.dart
@@ -33,3 +33,85 @@ enum Items {
   lolliteShovel,
   lolliteAxe
 }
+
+class ItemData {
+  final Tools toolType;
+
+  ItemData({this.toolType = Tools.none});
+
+  factory ItemData.getItemDataFor(Items item) {
+    switch (item) {
+      case Items.stoneSword:
+        return ItemData(toolType: Tools.sword);
+
+      case Items.diamondAxe:
+        return ItemData(toolType: Tools.axe);
+
+      case Items.diamondShovel:
+        return ItemData(toolType: Tools.axe);
+
+      case Items.diamondPickaxe:
+        return ItemData(toolType: Tools.pickaxe);
+
+      case Items.diamondSword:
+        return ItemData(toolType: Tools.sword);
+
+      case Items.goldenAxe:
+        return ItemData(toolType: Tools.axe);
+
+      case Items.goldenPickaxe:
+        return ItemData(toolType: Tools.pickaxe);
+
+      case Items.goldenShovel:
+        return ItemData(toolType: Tools.shovel);
+
+      case Items.goldenSword:
+        return ItemData(toolType: Tools.sword);
+
+      case Items.ironPickaxe:
+        return ItemData(toolType: Tools.pickaxe);
+
+      case Items.ironShovel:
+        return ItemData(toolType: Tools.shovel);
+
+      case Items.ironSword:
+        return ItemData(toolType: Tools.sword);
+
+      case Items.stoneAxe:
+        return ItemData(toolType: Tools.axe);
+
+      case Items.stonePickaxe:
+        return ItemData(toolType: Tools.pickaxe);
+
+      case Items.stoneShovel:
+        return ItemData(toolType: Tools.shovel);
+
+      case Items.woodenAxe:
+        return ItemData(toolType: Tools.axe);
+
+      case Items.woodenPickaxe:
+        return ItemData(toolType: Tools.pickaxe);
+
+      case Items.woodenShovel:
+        return ItemData(toolType: Tools.shovel);
+
+      case Items.woodenSword:
+        return ItemData(toolType: Tools.sword);
+
+      case Items.ironAxe:
+        return ItemData(toolType: Tools.sword);
+
+      case Items.lolliteAxe:
+        return ItemData(toolType: Tools.axe);
+      case Items.lollitePickaxe:
+        return ItemData(toolType: Tools.pickaxe);
+      case Items.lolliteShovel:
+        return ItemData(toolType: Tools.shovel);
+      case Items.lolliteSword:
+        return ItemData(toolType: Tools.sword);
+
+      default:
+        return ItemData();
+    }
+  }
+}
